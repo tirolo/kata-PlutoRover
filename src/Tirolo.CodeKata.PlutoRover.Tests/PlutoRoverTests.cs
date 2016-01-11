1e9507389463cb31778029d57b8251d677d6902d
Fernando Tirolo Gomes <tirolo@gmail.com>
2016-01-11 01:58:33 +0000

    Added test project and unit test framework dependencies

---
+++ src/Tirolo.CodeKata.PlutoRover.Tests/PlutoRoverTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tirolo.CodeKata.PlutoRover.Tests
+{
+    [TestFixture]
+    public class PlutoRoverTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
